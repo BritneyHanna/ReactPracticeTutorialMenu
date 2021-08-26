b489b5b1e37fdca6e4a8faff5e806f0173ee2f8e
BritneyHanna <b.ahanna@yahoo.com>
2021-08-26 14:02:56 -0400

    rearranged buttons- buttons are still manual ot dynamic

--- src/Categories.js
+++ src/Categories.js
@@ -4,10 +4,11 @@ const Categories = ({filterItems}) => {
   return <div className="btn-container">
     
     
-  <button className="filter-btn" onClick={()=>filterItems("breakfast")}> Breakfast</button>
   <button className="filter-btn" onClick={()=>filterItems("all")}> All</button>
-  <button className="filter-btn" onClick={()=>filterItems("shakes")}> Shakes</button>
+  <button className="filter-btn" onClick={()=>filterItems("breakfast")}> Breakfast</button>
   <button className="filter-btn" onClick={()=>filterItems("lunch")}> Lunch</button>
+
+  <button className="filter-btn" onClick={()=>filterItems("shakes")}> Shakes</button>
 
     </div>;
   
